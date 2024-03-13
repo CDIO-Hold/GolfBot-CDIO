b5088009b08b342f175db00bb9901cdedffde0e0
Zaid Sheikh <Zaidsh02@gmail.com>
2024-03-13 11:48:51 +0100

    Move to a destination and ball camera detection
    I dodn't code (ball camera detection) got it from a group member

--- GolfBot/main.py
+++ GolfBot/main.py
@@ -7,14 +7,52 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
 # Create your objects here.
 ev3 = EV3Brick()
 
+# Initialize the motors.
+left_wheel = Motor(Port.D)
+right_wheel = Motor(Port.A)
 
-# Write your program here.
-ev3.speaker.beep()
+
+# Initialize the robot
+Robot = DriveBase(left_wheel, right_wheel, wheel_diameter=65, axle_track=104)
+current_settings = robot.settings()
+new_settings = (500, current_settings[1], current_settings[2], current_settings[3])
+Robot.settings(new_settings[0], new_settings[1], new_settings[2], new_settings[3])
+
+
+# 
+def drive_to_dest(x, y):
+    # for now the robots coordinates
+    current_robot_x, current_robot_y = 0, 0
+
+    # The distance Robot needs to drive to reach destination 
+    distance_x = x - current_robot_x
+    distance_y = y - current_robot_y
+
+    # Calculate the angle to turn to
+    angle_to_turn = 50  #depends on where the (ball) is located
+
+    robot.turn(angle_to_turn)
+
+    # Drive straight to reach the coordinates
+    while Robot.distance() < distance_x:
+        Robot.straight(distance_x)
+        Robot.stop()
+        left_wheel.brake()
+        right_wheel.brake()
+
+        Robot.turn(angle_to_turn)
+
+        Robot.straight(distance_y)
+        Robot.stop()
+        left_wheel.brake()
+        right_wheel.brake()
+    
+    
+# 1 meter in x axis
+destination_x = 1000
+destination_y = 0
+
+drive_to_dest(destination_x, destination_y)
