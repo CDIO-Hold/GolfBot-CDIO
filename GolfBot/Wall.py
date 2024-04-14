fd0ef1a802443b12baf08f226c6d2e4bac2e8987
Zaid Sheikh <Zaidsh02@gmail.com>
2024-04-14 13:41:01 +0200

    Wall detection added and structured the YOLO file a bit better

---
+++ GolfBot/Wall.py
@@ -0,0 +1,7 @@
+class Wall:
+    def __init__(self, name, x1, y1, x2, y2):
+        self.name = name
+        self.x1 = x1 #Start
+        self.y1 = y1 #Start
+        self.x2 = x2 #End
+        self.y2 = y2 #End
--- GolfBot/YOLO.py
+++ GolfBot/YOLO.py
@@ -2,34 +2,36 @@ import math
 from ultralytics import YOLO
 import cv2
 import cvzone
-
 from GolfBot.Ball import Ball
-
-
-def detect_ball(confidence, class_name, x1, x2, y1, y2) -> Ball:
-    # Center coords
-    x = (x1 + x2) / 2
-    y = (y1 + y2) / 2
-
-    cvzone.putTextRect(img, f'{class_name[object]} {confidence:.2f}% x={x} y={y}', (max(0, x1), max(35, y1)), scale=1,
-                       thickness=1)
-    return Ball(class_name, x, y)
-
+from GolfBot.Wall import Wall
 
 # 1 if more webcams, 0 if only one cam
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-# Using someone elses data for now, will be creating our own
 # Using YOLOv8l, might change to YOLOv8x, since it is more accurate
+#model = YOLO('bane.pt') #Balls detection
+model = YOLO('field.pt') #Balls and walls
 
-model = YOLO('bane.pt')
+#model.predict("Bane.jpg", save=True)
 
-# model.predict("Bane.jpg", save=True)
+# className = ['white-ball', 'orange-ball', 'wall', 'goal', 'robot','robot_front', 'egg']
+#className = ['Back', 'robot', 'robot-front', 'white']  #Balls
+className = ['Back', 'Ball orange', 'Ball white', 'Bounds', 'Corner', 'Cross', 'Front', 'Robot'] # Balls and walls
 
-# objects = ['white-ball', 'orange-ball', 'wall', 'goal', 'robot', 'egg']
-className = ['orange', 'robot', 'robot-front', 'white']
+def detect_ball(class_name, x1, x2, y1, y2) -> Ball:
+    # Center coords
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+
+    return Ball(class_name, x, y)
+
+def detect_wall(class_name, x1, x2, y1, y2) -> Wall:
+    #start = (x1, y2)
+    #end = (x2, y2)
+
+    return Wall(class_name, x1, y2, x2, y2)
 
 while True:
     success, img = cap.read()
@@ -46,16 +48,24 @@ while True:
 
             confidence = math.ceil((box.conf[0] * 100)) / 100
 
-            object = int(box.cls[0])
+            object_detected = int(box.cls[0])
 
-            current_class = className[object]
+            current_class = className[object_detected]
 
             # Shows class name and confidence on screen
             text = ""
-            if current_class == "white" or current_class == "orange":
-                current_ball = detect_ball(confidence, current_class, x1, x2, y1, y2)
-            else:
-                cvzone.putTextRect(img, f'{className[object]} {confidence:.2f}%', (max(0, x1), max(35, y1)),
+            #if current_class == "white" or current_class == "orange": # for balls only
+            if current_class == "Ball white" or current_class == "Ball orange": #for balls and walls
+                current_ball = detect_ball(current_class, x1, x2, y1, y2)
+                cvzone.putTextRect(img, f'{current_class[object_detected]} {confidence:.2f}% x={current_ball.x} y={current_ball.y}',
+                                   (max(0, x1), max(35, y1)), scale=1,thickness=1)
+            elif current_class == 'Bounds': #walls
+                current_wall = detect_wall(current_class,x1,x2,y1,y2)
+                cvzone.putTextRect(img, f'{current_class[object_detected]} {confidence:.2f}% start={current_wall.x1, current_wall.y2} end={current_wall.x2, current_wall.y2}',
+                                   (max(0, x1), max(35, y1)), scale=1,
+                                   thickness=1)
+            elif confidence < 0.5:
+                cvzone.putTextRect(img, f'{current_class[object_detected]} {confidence:.2f}%', (max(0, x1), max(35, y1)),
                                    scale=1,
                                    thickness=1)
 
