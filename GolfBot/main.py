d037fab528dcb1540add3d632b15fee0583f67f8
zahed wafa <s224316@dtu.dk>
2024-04-28 23:08:17 +0200

    Drive to goal-destination

--- GolfBot/main.py
+++ GolfBot/main.py
@@ -6,17 +6,21 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from math import atan2, degrees, sqrt
+
 # Create the ev3
 ev3 = EV3Brick()
 
 # Initialize the motors
 left_wheel = Motor(Port.D)
 right_wheel = Motor(Port.A)
+right_motor_for_balls = Motor(Port.C)
+left_motor_for_balls = Motor(Port.B)
 
 
 # Initialize the robot
 Robot = DriveBase(left_wheel, right_wheel, wheel_diameter=65, axle_track=104)
-current_settings = robot.settings()
+current_settings = Robot.settings()
 new_settings = (500, current_settings[1], current_settings[2], current_settings[3])
 Robot.settings(new_settings[0], new_settings[1], new_settings[2], new_settings[3])
 
@@ -33,7 +37,7 @@ def drive_to_dest(x, y):
     # Calculate the angle to turn to
     angle_to_turn = 50  #depends on where the (ball) is located
 
-    robot.turn(angle_to_turn)
+    Robot.turn(angle_to_turn)
 
     # Drive straight to reach the coordinates
     while Robot.distance() < distance_x:
@@ -55,3 +59,20 @@ destination_x = 1000
 destination_y = 0
 
 drive_to_dest(destination_x, destination_y)
+
+# Drive to goal-destination
+def Drive_to_goal(x, y):
+    current_robot_x, current_robot_y = 0, 0
+
+    # The distance Robot needs to drive to reach destination
+    distance_x = x - current_robot_x
+    distance_y = y - current_robot_y
+
+    # Drive the robot to goal
+    Drive_to_goal(50, 500)
+
+    #Rotate the motor for the balls, for the balls to get into the goal
+    left_wheel.brake()
+    right_wheel.brake()
+    left_motor_for_balls.run(5) #Test, and see that the speed should be
+    right_motor_for_balls.run(5) #Test, and see that the speed should be
