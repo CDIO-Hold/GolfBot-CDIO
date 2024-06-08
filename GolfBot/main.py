c4953e5e1894cbdb54c012cf950e886e9c56452f
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-08 23:21:10 +0200

    Driver class implemented and testing rpyc

--- GolfBot/main.py
+++ GolfBot/main.py
@@ -7,52 +7,124 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+import math
+
+
+def value_or_default(value, default):
+    return default if value is None else default
+
+
+def degrees_to_radians(degrees: float):
+    return (degrees / 180) * math.pi
+
+
+def radians_to_degrees(radians: float):
+    return (radians / math.pi) * 180
+
+
+class Position:
+    def __init__(self, x, y, angle=None):
+        self.x = x
+        self.y = y
+        self.angle = angle
+
+    @property
+    def has_angle(self):
+        return self.angle is not None
+
+    def __str__(self):
+        return "(" + str(self.x) +"; " + str(self.y) + ")"
+
+
+class Driver:
+    wheel_diameter = 6.88
+    wheel_distance = 13.5
+
+    def __init__(self, left_motor, right_motor, drive_speed: float = 1000, rotate_speed: float = None):
+        self.drive_speed = drive_speed
+        self.rotate_speed = value_or_default(rotate_speed, drive_speed)
+
+        self.base = DriveBase(left_motor, right_motor, self.wheel_diameter * 10, self.wheel_distance * 10)
+        #self.base.settings(straight_speed=drive_speed)
+        self.rotation = 0.0
+        self.position = Position(0.0, 0.0)
+
+        self.wheel_circumference = self.wheel_diameter * math.pi
+        self.rotation_circumference = self.wheel_distance * math.pi
+
+    def set_rotation(self, angle):
+        self.rotation = angle % 360
+
+    def set_coordinates(self, x, y):
+        self.position = Position(x, y)
+
+    def turn(self, degrees):
+        """
+        if degrees > 180:
+            degrees = -(degrees - 180)
+        elif degrees < -180:
+            degrees = -(degrees + 180)
+        """
+
+        self.base.turn(-degrees)
+        self.rotation = (self.rotation + degrees) % 360
+
+    def turn_to(self, target_angle):
+        delta_angle = (target_angle - self.rotation)
+        self.turn(delta_angle)
+
+    def drive(self, distance):
+        self.base.straight(distance)
+        self.base.stop()
+
+        heading_radians = degrees_to_radians(self.rotation)
+        self.position.x += math.cos(heading_radians) * distance
+        self.position.y += math.sin(heading_radians) * distance
+
+    def drive_to(self, target_position: Position):
+        delta_x = target_position.x - self.position.x
+        delta_y = target_position.y - self.position.y
+        distance = math.sqrt(delta_x*delta_x + delta_y*delta_y)
+
+        drive_angle = radians_to_degrees(math.atan2(delta_y, delta_x))
+        print("Distance:", distance)
+        print("Drive angle:", drive_angle)
+        self.turn_to(drive_angle)
+
+        self.drive(distance)
+
+        if target_position.has_angle:
+            self.turn_to(target_position.angle)
+
 # Create the ev3
 ev3 = EV3Brick()
 
 # Initialize the motors
-left_wheel = Motor(Port.D)
-right_wheel = Motor(Port.A)
+right_wheel = Motor(Port.B)
+left_wheel = Motor(Port.C)
 
+# Initialize the driver
+driver = Driver(left_wheel, right_wheel)
 
-# Initialize the robot
-Robot = DriveBase(left_wheel, right_wheel, wheel_diameter=65, axle_track=104)
-current_settings = robot.settings()
-new_settings = (500, current_settings[1], current_settings[2], current_settings[3])
-Robot.settings(new_settings[0], new_settings[1], new_settings[2], new_settings[3])
-
-
-# This function gets two coordinates and drives the robot to the destination
-def drive_to_dest(x, y):
-    # for now the robots coordinates
-    current_robot_x, current_robot_y = 0, 0
-
-    # The distance Robot needs to drive to reach destination 
-    distance_x = x - current_robot_x
-    distance_y = y - current_robot_y
-
-    # Calculate the angle to turn to
-    angle_to_turn = 50  #depends on where the (ball) is located
-
-    robot.turn(angle_to_turn)
-
-    # Drive straight to reach the coordinates
-    while Robot.distance() < distance_x:
-        Robot.straight(distance_x)
-        Robot.stop()
-        left_wheel.brake()
-        right_wheel.brake()
-
-        Robot.turn(angle_to_turn)
-
-        Robot.straight(distance_y)
-        Robot.stop()
-        left_wheel.brake()
-        right_wheel.brake()
-    
-    
+path = [Position(1000, 0), Position(500, 300), Position(-200, 200), Position(0, 0)]
 # 1 meter in x axis
 destination_x = 1000
 destination_y = 0
 
-drive_to_dest(destination_x, destination_y)
+for point in path:
+    driver.drive_to(point)
+    print("Position:", driver.position)
+    print("Direction:", driver.rotation)
+
+if __name__ == "__main__":
+    import rpyc
+
+    conn = rpyc.classic.connect("192.168.124.17")
+
+    ev3dev2_motor = conn.modules['ev3dev2.motor']
+
+    motor = ev3dev2_motor.LargeMotor(ev3dev2_motor.OUTPUT_C)
+
+    #motor = conn.modules['ev3dev2.motor'].MediumMotor(conn.modules['ev3dev2.motor'].OUTPUT_D)
+
+    motor.run_forever(speed_sp=1000)
