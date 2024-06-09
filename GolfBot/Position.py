204617149e5fc3e1a7dbf76586a9d84501e82289
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-09 17:35:54 +0200

    removed the old Position class

--- GolfBot/Position.py
+++ GolfBot/Position.py
@@ -1,4 +1,12 @@
 class Position:
-    def __init__(self, x, y):
+    def __init__(self, x, y, angle=None):
         self.x = x
         self.y = y
+        self.angle = angle
+
+    @property
+    def has_angle(self):
+        return self.angle is not None
+
+    def __str__(self):
+        return "(" + str(self.x) +"; " + str(self.y) + ")"
