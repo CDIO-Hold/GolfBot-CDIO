17e73e82c734b6786bf4fe0e571a573f1ce4e85a
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-09 17:37:13 +0200

    updated the Robot class init

--- GolfBot/Robot.py
+++ GolfBot/Robot.py
@@ -1,12 +1,12 @@
 from GolfBot.Direction import Direction
 from GolfBot.Driver import Driver
-from GolfBot.Position import Position
+from GolfBot.Box import Box
 
 
 class Robot:
-    def __init__(self, driver: Driver, position: Position, facing, ball_count: int, is_moving: bool):
+    def __init__(self, driver: Driver, position: Box, facing: Direction, ball_count: int, is_moving: bool):
         self.driver = driver
         self.position = position
-        self.facing = Direction
+        self.facing = facing
         self.ball_count = ball_count
         self.is_moving = is_moving
