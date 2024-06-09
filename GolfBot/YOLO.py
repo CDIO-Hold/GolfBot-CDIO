11266ce3afb82e35b579dc5dacf4b3cfa15f16c1
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-09 17:39:33 +0200

    implemented function to get the coords of different objects

--- GolfBot/YOLO.py
+++ GolfBot/YOLO.py
@@ -3,39 +3,94 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 from GolfBot.Ball import Ball
+from GolfBot.Egg import Egg
 from GolfBot.Position import Position
+from GolfBot.Box import Box
 from GolfBot.Wall import Wall
+from GolfBot.Robot import Robot
+from GolfBot.Direction import Direction
+from GolfBot.Goal import Goal
 
 # 1 if more webcams, 0 if only one cam
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-# Using YOLOv8l, might change to YOLOv8x, since it is more accurate
-# model = YOLO('bane.pt') #Balls detection
-model = YOLO('field.pt')  # Balls and walls
+model = YOLO('best.pt')  # NEWEST Before 3 ugers
+#model.predict("Bane3.jpg", save=True)
 
-# model.predict("Bane.jpg", save=True)
 
-# className = ['white-ball', 'orange-ball', 'wall', 'goal', 'robot','robot_front', 'egg']
-# className = ['Back', 'robot', 'robot-front', 'white']  #Balls
-className = ['Back', 'Ball orange', 'Ball white', 'Bounds', 'Corner', 'Cross', 'Front', 'Robot']  # Balls and walls
+className = ['egg', 'goal', 'orange-ball', 'robot', 'robot-front', 'wall', 'white-ball']
 
 
 def detect_ball(class_name, x1, x2, y1, y2) -> Ball:
     # Center coords
     x = (x1 + x2) / 2
     y = (y1 + y2) / 2
-
     return Ball(class_name, Position(x, y))
 
 
 def detect_wall(class_name, x1, x2, y1, y2) -> Wall:
-    # start = (x1, y2)
-    # end = (x2, y2)
+    wall = Wall(class_name, start_position=Position(x1, y1), end_position=Position(x2, y2))
 
-    return Wall(class_name, start_position=Position(x1, y1), end_position=Position(x2, y2))
+    wall.is_left_wall = x1 < 640
+    wall.is_right_wall = x2 > 640
+    return wall
 
+
+robot = None
+
+def detect_robot(robot, x1, x2, y1, y2) -> Robot:
+    position = Box(x1, y1, x2, y2)
+
+    if robot is None:
+        ball_count = 0
+        is_moving = False
+        robot = Robot(None, position, None, ball_count, is_moving)
+    else:
+        robot.position = position
+
+    return robot
+
+
+def detect_robot_front(robot, x1, x2, y1, y2):
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+
+    # Center of the robots coords
+    robot_x = (robot.position.x1 + robot.position.x2) / 2
+    robot_y = (robot.position.y1 + robot.position.y2) / 2
+
+    #calculate the angle
+    angle = math.degrees(math.atan2(y - robot_y, x - robot_x))
+    direction = Direction(angle)
+    robot.facing = direction
+
+    return robot
+
+
+def detect_goal(name, x1, x2, y1, y2) -> Goal:
+    # Center coords
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    position = Position(x, y)
+
+    # Check if the goal is on left or right wall
+    return Goal(name, position, 3)
+
+def goal_on_wall(wall):
+    x = (wall.start_position.x + wall.end_position.x) / 2
+    y = (wall.start_position.y + wall.end_position.y) / 2
+    position = Position(x, y)
+
+    return Goal('goal', position, 3)
+
+
+def detect_egg(class_name, x1, x2, y1, y2) -> Egg:
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+
+    return Egg(class_name, Position(x, y))
 
 while True:
     success, img = cap.read()
@@ -58,25 +113,34 @@ while True:
 
             # Shows class name and confidence on screen
             text = ""
-            # if current_class == "white" or current_class == "orange": # for balls only
-            if current_class == "Ball white" or current_class == "Ball orange":  # for balls and walls
-                current_ball = detect_ball(current_class, x1, x2, y1, y2)
-                text = f'{current_class[object_detected]} {confidence:.2f}% \
-                    x={current_ball.position.x} y={current_ball.position.y}'
+        if current_class == "orange-ball" or current_class == "white-ball":
+            current_ball = detect_ball(current_class, x1, x2, y1, y2)
+            text = f'{current_class} {confidence:.2f}% x={current_ball.position.x} y={current_ball.position.y}'
+            cvzone.putTextRect(img, text, (max(0, x1), max(35, y1)), scale=1, thickness=1)
 
-                cvzone.putTextRect(img, text, (max(0, x1), max(35, y1)), scale=1, thickness=1)
-            elif current_class == 'Bounds':  # walls
-                current_wall = detect_wall(current_class, x1, x2, y1, y2)
-                text = f'{current_class[object_detected]} {confidence:.2f}% \
-                    start={current_wall.start_position.x, current_wall.start_position.y} \
-                    end={current_wall.end_position.x, current_wall.end_position.y}'
+        elif current_class == "wall":
+            current_wall = detect_wall(current_class, x1, x2, y1, y2)
+            text = f'{current_class} {confidence:.2f}% start={current_wall.start_position.x, current_wall.start_position.y} end={current_wall.end_position.x2, current_wall.end_position.y2}'
+            cvzone.putTextRect(img, text, (max(0, x1), max(35, y1)), scale=1, thickness=1)
 
+            if current_wall.is_left_wall or current_wall.is_right_wall:
+                goal = goal_on_wall(current_wall)
+                text = f'{current_class} {confidence:.2f}% x,y'
+                cvzone.putTextRect(img, text, (max(0, goal.position.x), max(35, goal.position.y)), scale=1, thickness=1)
+
+
+        elif current_class == "robot":
+            robot = detect_robot(robot, x1, x2, y1, y2)
+
+        elif current_class == "robot-front":
+            if robot is not None:
+                robot = detect_robot_front(robot, x1, x2, y1, y2)
+                text = f'{current_class} {confidence:.2f}% angle={robot.facing.angle:.2f}'
                 cvzone.putTextRect(img, text, (max(0, x1), max(35, y1)), scale=1, thickness=1)
-            elif confidence < 0.5:
-                cvzone.putTextRect(img, f'{current_class[object_detected]} {confidence:.2f}%',
-                                   (max(0, x1), max(35, y1)),
-                                   scale=1,
-                                   thickness=1)
+        elif current_class == "egg":
+            egg = detect_egg(current_class, x1, x2, y1, y2)
+            text = f'{current_class} {confidence:.2f}% x={egg.position.x} y={egg.position.y}'
+            cvzone.putTextRect(img, text, (max(0, x1), max(35, y1)), scale=1, thickness=1)
 
     cv2.imshow("image", img)
     cv2.waitKey(1)
