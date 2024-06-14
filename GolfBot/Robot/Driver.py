bb5f41f77088de4d558664dfe2885e107c1598f8
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-14 12:19:25 +0200

    added the classes from main

--- GolfBot/Robot/Driver.py
+++ GolfBot/Robot/Driver.py
@@ -1,71 +1,67 @@
 import math
-import pybricks.robotics
-
-
-def degrees_to_radians(degrees: float):
-    return (degrees / 180) * math.pi
-
-
-def radians_to_degrees(radians: float):
-    return (radians / math.pi) * 180
+from GolfBot.Shared import Position, Angle
+from .Speed import Speed
+from .Circle import Circle
 
 
 class Driver:
-    wheel_diameter = 6.88
-    wheel_distance = 13.5
+    def __init__(self,
+                 move_tank,
+                 speed: Speed,
+                 wheel: Circle,
+                 turn_circle: Circle,
+                 starting_rotation = 0.0,
+                 starting_position = (0, 0)):
+        self.tank = move_tank
+        self.speed = speed
+        self.wheel = wheel
+        self.turn_circle = turn_circle
 
-    def __init__(self, left_motor, right_motor, drive_speed: float = 1000, rotate_speed: float = None):
-        self.drive_speed = drive_speed
-        self.rotate_speed = drive_speed if rotate_speed is None else rotate_speed
+        if type(starting_rotation) is float:
+            self.rotation = Angle(starting_rotation, degrees)
+        else:
+            self.rotation = starting_rotation
 
-        self.base = pybricks.robotics.DriveBase(left_motor, right_motor, self.wheel_diameter * 10, self.wheel_distance * 10)
-        #self.base.settings(straight_speed=drive_speed)
-        self.rotation = 0.0
-        self.position = (0.0, 0.0)
-
-        self.wheel_circumference = self.wheel_diameter * math.pi
-        self.rotation_circumference = self.wheel_distance * math.pi
+        if type(starting_position) is tuple:
+            self.position = Position(starting_position[0], starting_position[1])
+        else:
+            self.position = starting_position
 
     def set_rotation(self, angle):
         self.rotation = angle % 360
 
-    def set_coordinates(self, x, y):
-        self.position = (x, y)
+    def set_coordinates(self, x: float, y: float):
+        self.position = Position(x, y)
 
     def turn(self, degrees):
-        """
-        if degrees > 180:
-            degrees = -(degrees - 180)
-        elif degrees < -180:
-            degrees = -(degrees + 180)
-        """
+        # Make the degree in the interval 0 - 360
+        degrees %= 360
 
-        self.base.turn(-degrees)
         self.rotation = (self.rotation + degrees) % 360
 
     def turn_to(self, target_angle):
-        delta_angle = (target_angle - self.rotation)
-        self.turn(delta_angle)
+        target_angle = (target_angle % 360)
+
+        self.tank.turn_degrees(self.speed.rotate_speed, target_angle)
 
     def drive(self, distance):
-        self.base.straight(distance)
-        self.base.stop()
+        rotations = distance / self.wheel.circumference
 
-        heading_radians = degrees_to_radians(self.rotation)
-        self.position.x += math.cos(heading_radians) * distance
-        self.position.y += math.sin(heading_radians) * distance
+        self.tank.on_for_rotations(
+            self.speed.straight_speed,
+            self.speed.straight_speed,
+            rotations
+        )
 
-    def drive_to(self, target_position):
-        delta_x = target_position.x - self.position[0]
-        delta_y = target_position.y - self.position[1]
-        distance = math.sqrt(delta_x*delta_x + delta_y*delta_y)
+        self.position.x += math.cos(self.rotation) * distance
+        self.position.y += math.sin(self.rotation) * distance
 
-        drive_angle = radians_to_degrees(math.atan2(delta_y, delta_x))
-        print("Distance:", distance)
-        print("Drive angle:", drive_angle)
+    def drive_to(self, target_position: Position):
+        delta_x = target_position.x - self.position.x
+        delta_y = target_position.y - self.position.y
+
+        drive_angle = math.acos(delta_x)
         self.turn_to(drive_angle)
 
+        distance = math.sqrt(delta_x*delta_x + delta_y*delta_y)
         self.drive(distance)
-
-        if target_position.has_angle:
-            self.turn_to(target_position.angle)
