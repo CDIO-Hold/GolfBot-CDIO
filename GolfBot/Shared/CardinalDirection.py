20c2d491eecd9fff3cd920ae0c400c50f4845b5f
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-18 00:55:26 +0200

    Created the function angle_to_cardinal

--- GolfBot/Shared/CardinalDirection.py
+++ GolfBot/Shared/CardinalDirection.py
@@ -6,3 +6,13 @@ class CardinalDirection(Enum):
     NORTH = 2
     WEST = 3
     SOUTH = 4
+
+    def angle_to_cardinal(angle):
+        if 45 <= angle < 135:
+            return CardinalDirection.NORTH
+        elif -45 <= angle < 45:
+            return CardinalDirection.EAST
+        elif angle >= 135 or angle < -135:
+            return CardinalDirection.WEST
+        else:
+            return CardinalDirection.SOUTH
