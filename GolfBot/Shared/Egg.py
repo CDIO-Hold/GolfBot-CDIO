851d2f0426942d38364a5b6875fdb325250a6561
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-17 10:48:19 +0200

    fixed the init of Egg class

--- GolfBot/Shared/Egg.py
+++ GolfBot/Shared/Egg.py
@@ -2,5 +2,6 @@ from .Position import Position
 
 
 class Egg:
-    def __init__(self, position: Position):
+    def __init__(self, name, position: Position):
+        self.name = name
         self.position = position
