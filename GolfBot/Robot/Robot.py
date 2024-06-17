31f6069ddcd5296061f9a269e9063bd4f3beaadd
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-18 00:55:04 +0200

    added Box to Robot

--- GolfBot/Robot/Robot.py
+++ GolfBot/Robot/Robot.py
@@ -1,9 +1,10 @@
 from .Driver import Driver
 from .Collector import Collector
+from ..Shared import Box
 
 
 class Robot:
-    def __init__(self, driver: Driver, collector: Collector, position, size, facing):
+    def __init__(self, driver: Driver, collector: Collector, position: Box, size, facing):
         self.driver = driver
         self.collector = collector
         self.position = position
