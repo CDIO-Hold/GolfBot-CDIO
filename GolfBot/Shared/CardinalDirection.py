7a7bc372fe2c107f629eb0b847247432c770e348
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-20 10:34:11 +0200

    made angle_to_cardinal static

--- GolfBot/Shared/CardinalDirection.py
+++ GolfBot/Shared/CardinalDirection.py
@@ -7,7 +7,8 @@ class CardinalDirection(Enum):
     WEST = 3
     SOUTH = 4
 
-    def angle_to_cardinal(self, angle):
+    @staticmethod
+    def angle_to_cardinal(angle):
         if 45 <= angle < 135:
             return CardinalDirection.NORTH
         elif -45 <= angle < 45:
