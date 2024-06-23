e606f12639ad5fe01fa934b3392c811a05fb2b18
Zaid Sheikh <Zaidsh02@gmail.com>
2024-06-23 18:15:11 +0200

    made egg a bix instead of a vector

--- GolfBot/Basics/Egg.py
+++ GolfBot/Basics/Egg.py
@@ -1,7 +1,7 @@
-from .Vector import Vector
+from .Box import Box
 
 
 class Egg:
-    def __init__(self, name, position: Vector):
+    def __init__(self, name, position: Box):
         self.name = name
         self.position = position
